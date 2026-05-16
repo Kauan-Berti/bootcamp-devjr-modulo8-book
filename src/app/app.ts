c9ced31549736bc3986d2d1aada3ca75baec603a
Kauan Berti <kauan.g.berti@gmail.com>
2026-05-16 17:01:49 -0300

    added BootStrap

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('bootcamp-devjr-modulo8-book');
+  title = 'Lisvros S/A';
 }
